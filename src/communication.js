992e9a5de4737c80d7d805a7e89d77051251c9f1
lewis <lewisgarmston@gmail.com>
2017-04-25 20:20:11 -0700

    Added Serialization and feet

--- src/communication.js
+++ src/communication.js
@@ -1,23 +1,4 @@
-// TODO: move to Player
-function deserialize(playerData) {
-	console.assert(playerData.position.x !== undefined);
-	console.assert(playerData.position.y !== undefined);
-	console.assert(playerData.position.z !== undefined);
 
-	console.assert(playerData.head.quaternion.length === 4)
-	console.assert(playerData.head.quaternion.every(el => el !== undefined))
-	return {
-		position: new THREE.Vector3(
-			playerData.position.x,
-			playerData.position.y,
-			playerData.position.z
-		),
-		color: new THREE.Color(playerData.color),
-		head: {
-			quaternion: (new THREE.Quaternion()).fromArray(playerData.head.quaternion)
-		}
-	};
-}
 
 class Communication {
 	constructor(emitter) {
@@ -56,40 +37,40 @@ class Communication {
 
 			this.ds.on('error', () => {
 				// server restarted?
-				setTimeout(function() {
+				setTimeout(function () {
 					window.location.reload();
 				}, 1000);
 			});
 		})
-		.then(() => {
-			this.playersRecord = this.ds.record.getRecord(`players`);
-			this.gameRecord = this.ds.record.getRecord('game');
-			this.hackysackRecord = this.ds.record.getRecord('hackysack');
-			this.hackysackRecord.subscribe('position', value => {
-				this.hackysackPosition = value;
-			});
+			.then(() => {
+				this.playersRecord = this.ds.record.getRecord(`players`);
+				this.gameRecord = this.ds.record.getRecord('game');
+				this.hackysackRecord = this.ds.record.getRecord('hackysack');
+				this.hackysackRecord.subscribe('position', value => {
+					this.hackysackPosition = value;
+				});
 
-			this.playersRecord.subscribe('playerData', (playersUpdate) => {
-				console.assert(checkPos(playersUpdate));
-				this.latestPlayersUpdate = playersUpdate;
-			});
+				this.playersRecord.subscribe('playerData', (playersUpdate) => {
+					console.assert(checkPos(playersUpdate));
+					this.latestPlayersUpdate = playersUpdate;
+				});
 
-			this.gameRecord.subscribe('forceRefresh', (doIt) => {
-				if (doIt) {
-					this.gameRecord.set('forceRefresh', false);
-					window.location.reload();
-				}
+				this.gameRecord.subscribe('forceRefresh', (doIt) => {
+					if (doIt) {
+						this.gameRecord.set('forceRefresh', false);
+						window.location.reload();
+					}
+				});
+			})
+			.then(() => {
+				return this.detectGame();
 			});
-		})
-		.then(() => {
-			return this.detectGame();
-		});
 	}
 
 	setCallbacks(callbacks) {
 		this.callbacks = callbacks;
 	}
-	
+
 	detectGame() {
 		return new Promise((resolve) => {
 			this.ds.record.snapshot('game', (error, game) => {
@@ -101,6 +82,10 @@ class Communication {
 
 	sendNewGame() {
 		this.gameRecord.set('inProgress', true);
+	}
+
+	sendEndGame() {
+		this.gameRecord.set('inProgress', false);
 	}
 
 	sendHackysackPosition(hackysackPosition) {
@@ -127,8 +112,9 @@ class Communication {
 				continue;
 			}
 
-			const playerUpdate = deserialize(this.latestPlayersUpdate[clientId]);
-			
+			//const playerUpdate = deserialize(this.latestPlayersUpdate[clientId]);
+			const playerUpdate = Player.deserialize(this.latestPlayersUpdate[clientId]);
+
 			if (!playersInScene[clientId]) {
 				const playerEntered = playerUpdate;
 				playerEntered.clientId = clientId;
--- src/player.js
+++ src/player.js
@@ -1,7 +1,7 @@
 function getRandomColor() {
     var letters = '0123456789ABCDEF';
     var color = '#';
-    for (var i = 0; i < 6; i++ ) {
+    for (var i = 0; i < 6; i++) {
         color += letters[Math.floor(Math.random() * 16)];
     }
     return new THREE.Color(color);
@@ -14,8 +14,8 @@ class Player extends THREE.Object3D {
         this.communication = communication;
 
         this.config = {
-            HAND_SIZE: 0.1, 
-            HEAD_SIZE: 0.3, 
+            HAND_SIZE: 0.1,
+            HEAD_SIZE: 0.3,
             HEAD_HEIGHT: 1.5
         }
         this.head = "";
@@ -30,14 +30,42 @@ class Player extends THREE.Object3D {
         this.createPrefab(spawnLocation, lookatObject);
     }
 
+    static serialize(playerObject) {
+        var serializedPlayer = NetworkTransform.serialize(playerObject);
+        serializedPlayer.head = NetworkTransform.serialize(playerObject.head);
+        serializedPlayer.leftHand = NetworkTransform.serialize(playerObject.leftHand);
+        serializedPlayer.rightHand = NetworkTransform.serialize(playerObject.rightHand);
+        serializedPlayer.color = playerObject.color;
 
+        return serializedPlayer;
+    }
+
+
+    static deserialize(playerData) {
+        console.assert(playerData.position.x !== undefined);
+        console.assert(playerData.position.y !== undefined);
+        console.assert(playerData.position.z !== undefined);
+
+        console.assert(playerData.head.quaternion.length === 4)
+        console.assert(playerData.head.quaternion.every(el => el !== undefined))
+
+        var player = NetworkTransform.deserialize(playerData);
+        player.color = new THREE.Color(playerData.color);
+        player.head = NetworkTransform.deserialize(playerData.head);
+        player.leftHand = NetworkTransform.deserialize(playerData.leftHand);
+        player.rightHand = NetworkTransform.deserialize(playerData.rightHand);
+
+        return player;
+    }
+
+    
     createHead() {
         const headGeometry = new THREE.BoxGeometry(this.config.HEAD_SIZE, this.config.HEAD_SIZE, this.config.HEAD_SIZE);
         this.headMesh = new THREE.Mesh(headGeometry, new THREE.MeshLambertMaterial({ color: this.color }));
         this.head.add(this.headMesh);
     }
 
-    showHead(){
+    showHead() {
         this.headMesh.visible = true;
     }
 
@@ -48,18 +76,18 @@ class Player extends THREE.Object3D {
 
     createPrefab(spawnLocation, lookatObject) {
         this.head = new THREE.Group();
-        this.head.position.set(0,this.config.HEAD_HEIGHT, 0);
+        this.head.position.set(0, this.config.HEAD_HEIGHT, 0);
         this.add(this.head);
         this.createHead();
-       
+
         if (this.isLocalPlayer) {
             this.camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.1, 1000);
             this.head.add(this.camera);
         }
-        
+
         this.addControllers(this.head);
         this.showHead();
-        
+
         this.position.copy(spawnLocation);
 
         // test if we're syncing rotations
@@ -78,13 +106,15 @@ class Player extends THREE.Object3D {
         }
     }
 
-    updatePhysicsModel(modelObject, object){
+    updatePhysicsModel(modelObject, object) {
         modelObject.position.set(object.x, object.y, object.z);
-        //modelObject.position.copy(object.position);
-		//modelObject.quaternion.copy(object.quaternion);
+        modelObject.position.copy(object.position);
+        modelObject.quaternion.copy(object.quaternion);
     }
 
     addControllers(head) {
+
+        //TODO: check if the machine is VR capable here too!
         if (this.isLocalPlayer) {
             this.controls = new THREE.VRControls(head, (err) => {
                 console.error('VRControls', err);
@@ -105,12 +135,21 @@ class Player extends THREE.Object3D {
         }
 
         //put the hNDS ON THE FLOOR.
-        this.leftHand.position.set(0,this.config.HAND_SIZE, 0);
-        this.rightHand.position.set(0,this.config.HAND_SIZE, 0);
+        this.leftHand.position.set(0, this.config.HAND_SIZE, 0);
+        this.rightHand.position.set(0, this.config.HAND_SIZE, 0);
 
-        const controllerMesh = this.buildControllerMesh();
-        this.leftHand.add(controllerMesh.clone());
-        this.rightHand.add(controllerMesh.clone());
+        //const controllerMesh = this.buildControllerMesh();
+        //this.leftHand.add(controllerMesh.clone());
+        //this.rightHand.add(controllerMesh.clone());
+        this.buildControllerMeshFromObj(this.leftHand);
+        this.buildControllerMeshFromObj(this.rightHand);
+
+
+        //position feet nicely apart
+        this.leftHand.position.set(-0.2, 0, 0);
+        this.rightHand.position.set(0.2, 0, 0);
+        this.leftHand.quaternion.setFromEuler(new THREE.Euler(0, 25, 0, 'XYZ'));
+        this.rightHand.quaternion.setFromEuler(new THREE.Euler(0, -25, 0, 'XYZ'));
 
         this.leftHandModel = this.addControllerPhysicsBody(this.leftHand);
         this.rightHandModel = this.addControllerPhysicsBody(this.rightHand);
@@ -119,25 +158,60 @@ class Player extends THREE.Object3D {
         this.add(this.rightHand);
     }
 
-    addControllerPhysicsBody(controller){
+    addControllerPhysicsBody(controller) {
         var body = new CANNON.Body({
-			mass: 1,
+            mass: 1,
             type: CANNON.Body.Kinematic,
-			shape: new CANNON.Box(new CANNON.Vec3( this.config.HAND_SIZE, this.config.HAND_SIZE, this.config.HAND_SIZE)),
-			material: new CANNON.Material()
-		});
-		
-		//body.position.copy(controller.position);
-		//body.quaternion.copy(controller.quaternion);
-       
-        //this.physics.addHand(body);
+            shape: new CANNON.Box(new CANNON.Vec3(this.config.HAND_SIZE, this.config.HAND_SIZE, this.config.HAND_SIZE)),
+            material: new CANNON.Material()
+        });
+
+        //todo - convert to world coordinates!
+        body.position.copy(controller.position);
+        body.quaternion.copy(controller.quaternion);
+
+        this.physics.addHand(body);
 
         return body;
     }
 
     buildControllerMesh() {
         const controllerGeometry = new THREE.BoxGeometry(this.config.HAND_SIZE, this.config.HAND_SIZE, this.config.HAND_SIZE);
-        const controllerMaterial = new THREE.Mesh(controllerGeometry, new THREE.MeshLambertMaterial({ color: 0xe28d16 }));
+        const controllerMaterial = new THREE.Mesh(controllerGeometry, new THREE.MeshLambertMaterial({ color: this.color }));
         return controllerMaterial;
     }
+
+    buildControllerMeshFromObj(controller) {
+        var loader = new THREE.OBJLoader();
+        // loader.setMaterials([
+        //     new THREE.MeshLambertMaterial({ color: 0xe28d16 }), 
+        //     new THREE.MeshLambertMaterial({ color: 0x000000 }), 
+        //     new THREE.MeshLambertMaterial({ color: 0xffffff })
+        // ]);
+        var material = new THREE.MeshLambertMaterial({ color: this.color });
+        loader.load("models/MarioShoe.obj", object => {
+            console.log("loaded shoe");
+            object.traverse(function (child) {
+                if (child instanceof THREE.Mesh) child.material = material;
+            });
+
+            //LG: set this position to make it fit on the hand
+            object.position.set(0, 0.1, 0);
+            //object.scale.set(0.01,0.01,0.01);
+            controller.add(object);
+        });
+    }
+
+    // buildControllerMesh2(object) {
+    //     return new Promise(resolve => {
+    //         var loader = new THREE.OBJLoader();
+    //         loader.load(
+    //             'models/mariosshoe.obj',
+    //             object => {
+    //                 resolve();
+    //                 scene.add(object);
+    //             }
+    //         );
+    //     });
+    // }
 }
